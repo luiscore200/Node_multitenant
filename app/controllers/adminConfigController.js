a4c89375dca6d9dc8b0a750bbf436f18f66758a8
luiscore200 <luedco2009@gmail.com>
2024-08-18 11:22:22 -0500

    fix

--- app/controllers/adminConfigController.js
+++ app/controllers/adminConfigController.js
@@ -170,7 +170,9 @@ if(update['subscriptions']){
   if(suscripciones.length>0){
     suscripciones.forEach((subscription) => {
       const imagePath = findPath(subscription.image);
+      console.log(imagePath);
       if (imagePath) { 
+        
         subscription.image = imagePath;
       }
     });
