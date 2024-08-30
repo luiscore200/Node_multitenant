e226c69805f6a96023b2b94a65085e616a2ea543
luiscore200 <luedco2009@gmail.com>
2024-08-29 19:49:08 -0500

    fix

--- app/src/public/script.js
+++ app/src/public/script.js
@@ -2,9 +2,8 @@ exports.generateDynamicScript = (token, raffle  , asignaciones,userID) => {
 
     const itemsPerPage=250;
     
-    const separados = asignaciones.filter(obj => obj.status === "separado").map(obj => Number(obj.number));
-    const pagados = asignaciones.filter(obj => obj.status === "pagado").map(obj => Number(obj.number));
-    
+    const separados = asignaciones.filter(obj => obj.status==="separado").map(obj => obj.number);
+    const pagados = asignaciones.filter(obj => obj.status==="pagado").map(obj => obj.number);
     //const totalNums=Number(raffle.numbers);
     const totalNums=raffle.numbers;
     const price=raffle.price;
@@ -366,20 +365,40 @@ exports.generateDynamicScript = (token, raffle  , asignaciones,userID) => {
                     
                 };
 
-                          const renderCell = (number) => {
+               const renderCell = (number) => {
                     const cell = document.createElement('div');
-                    cell.id = 'cell-' + number;
-
-                    if (pagados.includes(number)) {
-                        cell.classList.add('assignedCell');
-                    } else if (separados.includes(number)) {
-                        cell.classList.add('reservedCell');
-                    } else {
-                        cell.classList.add('cell');
+                    cell.id='cell-'+number;
+                    if(pagados.includes(number)){
+                      cell.classList.add('assignedCell');
+                    }else   if(separados.includes(number)){
+                      cell.classList.add('reservedCell');
+                    } else{
+                     cell.classList.add('cell');
                     }
+                   
 
-                    cell.innerText = number;
-                    cell.addEventListener('click', () => select(number));
+                    const textCell = document.createElement('p');
+                    textCell.classList.add('cellText')
+                    textCell.innerText = number;
+
+                    cell.appendChild(textCell);
+
+                    if (!pagados.includes(number) && !separados.includes(number)) {
+                        cell.addEventListener('click', () => select(number));
+                          cell.addEventListener('mouseenter', () => {
+                            cell.classList.remove('cell');
+                            cell.classList.add('cell_hover');
+                            });
+                        cell.addEventListener('mouseleave', () => {
+                            cell.classList.remove('cell_hover');
+                            cell.classList.add('cell');
+                            });
+
+
+
+                    }
+                        
+                    
                     return cell;
                 };
 
